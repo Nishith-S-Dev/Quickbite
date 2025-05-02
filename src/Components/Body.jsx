7c532694cf0d4910678d2f42660c26f3a9ccde1e
Nishith-S-Dev <nishith.s.acharya@gmail.com>
2025-05-02 16:01:06 +0530

    correct the search functionality with restruant list fix

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,19 +1,33 @@
-import React ,{useState} from "react"
+import React ,{useState,useEffect} from "react"
 import RestrauntCard from "./RestruantCard";
-import {ResId} from "./config"
 
-const Restruant = ResId;
+
+
 const Body = ()=>{
     const [SearchText,setSearchText] = useState("");
 
-    const [RestruantList , setRestrauntList ]= useState(Restruant);
+    const [RestruantList,setRestrauntList ]= useState([]);
+    const [filteredRestrauntList , setFilteredRestrauntList ]= useState([]);
+  
+
+    const ApiCall = async()=>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        setFilteredRestrauntList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setRestrauntList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+    
     console.log("Render")
-    const filteredRestraunt =(searchText,restruant)=>{
-        const filterData = restruant.filter((e)=>{
+    const filteredRestraunt =(searchText,RestruantList)=>{
+        const filterData = RestruantList.filter((e)=>{
             return e.info.name.toLowerCase().includes(searchText.toLowerCase())
         })
         return filterData;
     }
+    useEffect(()=>{
+        ApiCall();
+    },[]);
+   if (!RestruantList) return null;
     return(
         <>   
             <div className="Search-Bar" style={{margin:"20px",  display:"flex",gap:"22px"}}>
@@ -23,18 +37,20 @@ const Body = ()=>{
             
 
             <button onClick={()=>{
-                const data = filteredRestraunt(SearchText,Restruant);
-                    setRestrauntList(data);
+                const data = filteredRestraunt(SearchText,RestruantList);
+                setFilteredRestrauntList(data);
 
             }}>Search</button>
             </div>
 
             <h1>Some Good Restraunt around you ...</h1>
-            <div style={{display:"flex",flexWrap:"wrap",justifyContent:"center"}}>
+            <div style={{display:"flex",flexWrap:"wrap",justifyContent:"center",gap:"20px"}}>
 
-            {RestruantList.map((data)=>{
+            {filteredRestrauntList.map((data)=>{
                 return(
-                    <RestrauntCard key={data.id} data={data.info}/>
+                    
+                    <RestrauntCard key={data.info.id } data={data.info}/>
+                    
                 )
             })}
             </div>
